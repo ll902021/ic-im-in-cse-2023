e7b110e918326104861d291f0944be46f2ba4433
ll902021 <yeeting921122@gmail.com>
2023-08-17 23:51:28 +0800

    js test

--- Signup/signup.js
+++ Signup/signup.js
@@ -1,6 +1,7 @@
 alert('有載入js 1');
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-database.js";
 
 // import { initializeApp } from './firebase/app';
 // import { getDatabase } from "./firebase/database";
