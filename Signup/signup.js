79bc2a0206398bfeb6af892cc2d98bfe3e96329d
ll902021 <yeeting921122@gmail.com>
2023-08-18 00:55:56 +0800

    db test

--- Signup/signup.js
+++ Signup/signup.js
@@ -1,12 +1,6 @@
-alert('有載入js 1');
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
 import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-database.js";
 
-// import { initializeApp } from './firebase/app';
-// import { getDatabase } from "./firebase/database";
-
-alert('有載入js');
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -19,19 +13,39 @@ const firebaseConfig = {
   appId: "1:589513037614:web:d38e85f91294bf6819ccbf"
 };
 
-//firebase.initializeApp(firebaseConfig);
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
+//const db = getDatabase(app);
 
-function writeUserData(userId, name, email, imageUrl) {
+function writeUserData(name, id, sex, food, idcard, birth, phone, size, emgname, emgphone, relation) {
   const db = getDatabase();
-  set(ref(db, 'users/' + userId), {
-    username: name,
-    email: email,
-    profile_picture : imageUrl
+  set(ref(db, `/${id}` + name), {
+    性別: sex,
+    葷素: food,
+    身分證字號: idcard,
+    生日: birth,
+    聯絡電話: phone,
+    尺寸: size,
+    緊急聯絡人姓名: emgname,
+    緊急聯絡人電話: emgphone,
+    緊急聯絡人關係: relation
   });
 }
 
-writeUserData("a","b","c","d");
+function processFormData(){
+  const formElement = document.getElementById('form');
+  const name = formElement[0].value;
+  const id = formElement[1].value;
+  const sex = formElement[2].value;
+  const food = formElement[3].value;
+  const idcard = formElement[4].value;
+  const birth = formElement[5].value;
+  const phone = formElement[6].value;
+  const size = formElement[7].value;
+  const emgname = formElement[8].value;
+  const emgphone = formElement[9].value;
+  const relation = formElement[10].value;
+
+  writeUserData(name, id, sex, food, idcard, birth, phone, size, emgname, emgphone, relation);
+}
