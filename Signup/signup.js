3df1d02edbc4248f5ef04f257f40b523bb19b6a6
ll902021 <yeeting921122@gmail.com>
2023-08-18 18:39:13 +0800

    db test

--- Signup/signup.js
+++ Signup/signup.js
@@ -38,17 +38,17 @@ function start(){
 function processFormData1(){
   alert("到底？？");
   const formElement = document.getElementById('form1');
-  const name = doucumect.getElementById('name').value;
-  const id = doucumect.getElementById('id').value;
-  const sex = document.querySelector('input[name="sex"]:checked').value;
-  const food = document.querySelector('input[name="food"]:checked').value;
-  const idcard = doucumect.getElementById('idcard').value;
-  const birth = doucumect.getElementById('birth').value;
-  const phone = doucumect.getElementById('phone').value;
-  const size = doucumect.getElementById('size').value;
-  const emgname = doucumect.getElementById('emgname').value;
-  const emgphone = doucumect.getElementById('emgphone').value;
-  const relation = doucumect.getElementById('relation').value;
+  const name = formElement[0].value;
+  const id = formElement[1].value;
+  const sex = formElement[2].value;
+  const food = formElement[3].value;
+  const idcard = formElement[4].value;
+  const birth = formElement[5].value;
+  const phone = formElement[6].value;
+  const size = formElement[7].value;
+  const emgname = formElement[8].value;
+  const emgphone = formElement[9].value;
+  const relation = formElement[10].value;
 
   writeUserData(name, id, sex, food, idcard, birth, phone, size, emgname, emgphone, relation);
 
@@ -57,21 +57,19 @@ function processFormData1(){
 function processFormData2(){
   alert("到底？？");
   const formElement = document.getElementById('form2');
-  const name = doucumect.getElementById('nname').value;
-  const id = doucumect.getElementById('iid').value;
-  const sex = document.querySelector('input[name="sex"]:checked').value;
-  const food = document.querySelector('input[name="food"]:checked').value;
-  const idcard = doucumect.getElementById('iidcard').value;
-  const birth = doucumect.getElementById('bbirth').value;
-  const phone = doucumect.getElementById('ppphone').value;
-  const size = doucumect.getElementById('ssize').value;
-  const emgname = doucumect.getElementById('emgnname').value;
-  const emgphone = doucumect.getElementById('emgppphone').value;
-  const relation = doucumect.getElementById('rrelation').value;
+  const name = formElement[0].value;
+  const id = formElement[1].value;
+  const sex = formElement[2].value;
+  const food = formElement[3].value;
+  const idcard = formElement[4].value;
+  const birth = formElement[5].value;
+  const phone = formElement[6].value;
+  const size = formElement[7].value;
+  const emgname = formElement[8].value;
+  const emgphone = formElement[9].value;
+  const relation = formElement[10].value;
 
   writeUserData(name, id, sex, food, idcard, birth, phone, size, emgname, emgphone, relation);
-
-
 
 }
 
@@ -80,15 +78,15 @@ function writeUserData(name, id, sex, food, idcard, birth, phone, size, emgname,
   alert("有載入4");
   const db = getDatabase();
   set(ref(db, `/${id}` + name), {
-    sex: sex,
-    food: food,
-    idcard: idcard,
-    birth: birth,
-    phone: phone,
-    size: size,
-    emgname: emgname,
-    emgphone: emgphone,
-    relation: relation
+    性別: sex,
+    葷素: food,
+    身分證字號: idcard,
+    生日: birth,
+    聯絡電話: phone,
+    尺寸: size,
+    緊急聯絡人姓名: emgname,
+    緊急聯絡人電話: emgphone,
+    與緊急聯絡人關係: relation
   })
 
   .then(function () {
